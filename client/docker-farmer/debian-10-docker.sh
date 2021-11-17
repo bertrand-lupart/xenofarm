2c95940ebd5c1a59e6470332aaf14bbada3ecb40
Bertrand Lupart <bertrand@gstack.io>
2021-11-17 11:27:26 +0100

    Add MySQL / MariaDB support

--- client/docker-farmer/debian-10-docker.sh
+++ client/docker-farmer/debian-10-docker.sh
@@ -2,6 +2,7 @@
 
 apt-get update && apt-get upgrade -y
 
-apt-get install -y make gcc g++ libgmp-dev wget curl nettle-dev nettle-bin
+apt-get install -y make gcc g++ libgmp-dev wget curl nettle-dev nettle-bin \
+  libmariadbd-dev
 
 ./client.sh
