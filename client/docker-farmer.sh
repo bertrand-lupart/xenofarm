bdf94986d6cf89203b9e44129b7541108a9adf92
Bertrand Lupart <bertrand@gstack.io>
2021-11-18 20:42:21 +0100

    Pull Pike worker container image before run them

--- client/docker-farmer.sh
+++ client/docker-farmer.sh
@@ -13,6 +13,7 @@ tags=$(curl --silent --fail --location --show-error \
   | jq --raw-output '.results[].name')
 
 for tag in ${tags}; do
+  docker pull "${user}/${repo}:${tag}"
   docker run --rm \
     --volume="$(pwd):/pikefarm" --workdir="/pikefarm" \
     --hostname="${tag}-docker" "${user}/${repo}:${tag}" "./client.sh"
