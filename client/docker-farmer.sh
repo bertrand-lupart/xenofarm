df96215c51b9445e7aab89df4de2a04d35befdb5
Bertrand Lupart <bertrand@gstack.io>
2021-11-21 13:44:45 +0100

    Filesystem tricks warning for docker-farm runners

--- client/docker-farmer.sh
+++ client/docker-farmer.sh
@@ -8,6 +8,24 @@ set -e
 user="bertrandlupart"
 repo="pikefarm-worker"
 
+
+# xenofarm user in pikefarm-worker containers
+uid=817
+gid=817
+
+echo "xenofarm user in container images is ${uid}:${gid}"
+echo "You may need to allow it to write at the root of this dir (PID file) :"
+echo "  chgrp ${gid} ."
+echo "  chmod g+w ."
+echo "You may also allow it to download and compile in the project dirs"
+echo "if your Docker host run Pikefarm project :"
+echo "  chgrp -R ${gid} pike-* project_*"
+echo "  chmod -R g+w pike-* project_*"
+echo "These ugly tricks should be made obsolete by smarter improvements "
+echo "in client.sh"
+
+
+
 tags=$(curl --silent --fail --location --show-error \
   "https://hub.docker.com/v2/repositories/${user}/${repo}/tags" \
   | jq --raw-output '.results[].name')
